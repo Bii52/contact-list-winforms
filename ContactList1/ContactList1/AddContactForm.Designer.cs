ccdc4484d2552e1bc72ab53743477bcf298c9e2d
bao kiet <baokie2004@gmail.com>
2025-03-29 20:35:19 +0700

    Sửa đổi AddContactForm

--- ContactList1/ContactList1/AddContactForm.Designer.cs
+++ ContactList1/ContactList1/AddContactForm.Designer.cs
@@ -103,11 +103,11 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(469, 118);
+            this.label4.Location = new System.Drawing.Point(514, 162);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(113, 16);
             this.label4.TabIndex = 7;
-            this.label4.Text = "Thong tin bổ sung";
+            this.label4.Text = "Thông tin bổ sung";
             // 
             // AddContactForm
             // 
